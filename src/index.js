b792e55a43065ae143777bb1eab8b7649099c9e8
Alberto Madrazo <betomadrazo@gmail.com>
2020-12-04 15:27:34 -0600

    Add React and files and folders I remember from the lost project.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Is there anybody out there?")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = 'React with Webpack and Babel'
+
+ReactDOM.render(<div>{title}</div>, document.getElementById('root'))
